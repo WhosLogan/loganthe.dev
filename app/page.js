0e9ec786017e25423beecc54c431423942b8957c
logan <logan@loganthe.dev>
2023-11-13 11:43:19 -0500

    Make the "under construction" scale to mobile

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
       <main>
           <div className="flex h-screen items-center justify-center">
-              <h1 className="text-5xl">Under Construction {'\u{1F6A7}'}</h1>
+              <h1 className="text-3xl md:text-5xl">Under Construction {'\u{1F6A7}'}</h1>
           </div>
       </main>
   )
