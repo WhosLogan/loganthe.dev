bfef8b3261c6fe9be652e4c90c8c3e91b49fe8b8
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-06-30 17:54:38 -0400

    Add under construction homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Under Construction</h1>
+<p>This website is currently under construction, it'll be done soon!</p>
