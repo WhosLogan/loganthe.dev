078ee18bb5acf2a92c7fc57f6c999302ebcb7680
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-09 11:33:35 -0400

    Add TailwindCSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,7 @@
+<script>
+    import "../app.css";
+</script>
+
 <svelte:head>
     <title>Logan's Portfolio</title>
 </svelte:head>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +10,8 @@ const config = {
 		adapter: adapter({
 			pages: ".svelte-kit/cloudflare"
 		})
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
