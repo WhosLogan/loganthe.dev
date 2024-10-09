2c5602c48e2cbd691d0b7a72dc829b7025d446eb
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-09 14:33:16 -0400

    Add basic content box

---
+++ src/lib/ContentBox.svelte
@@ -0,0 +1,13 @@
+<script>
+    let className = "";
+    let title = "";
+
+    export {className as class, title};
+</script>
+
+<div class="bg-neutral-800/80 {className} p-3 max-w-96 border border-white/60 rounded">
+    <div class="w-1/2 border-b mb-3">
+        <h1 class="text-lg font-bold">{title}</h1>
+    </div>
+    <slot />
+</div>
