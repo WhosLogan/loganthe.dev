34720eeaef4f8fd9d7631b4c12ad07ccd008cf9b
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-09 21:55:41 -0400

    Fix content box background

--- src/lib/ContentBox.svelte
+++ src/lib/ContentBox.svelte
@@ -6,7 +6,7 @@
     export {className as class, title};
 </script>
 
-<div class="bg-neutral-800/80 {className} p-3 max-w-96 border border-white/60 rounded">
+<div class="bg-neutral-800/40 {className} p-3 max-w-96 border border-white/40 rounded">
     <div class="w-1/2 border-b mb-3">
         <h1 class="text-lg font-bold">{title}</h1>
     </div>
