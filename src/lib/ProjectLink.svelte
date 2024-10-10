c2f688300c6678e74d6710f2f20ae66433116996
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-09 21:55:31 -0400

    Add basic project card/link

---
+++ src/lib/ProjectLink.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let title;
+    export let description;
+    export let link;
+    export let lang;
+</script>
+
+<a href="{link}" target="_blank" rel="noopener noreferrer">
+    <div class="p-5 bg-neutral-800 hover:bg-neutral-700/60 h-40 flex flex-col rounded">
+        <p class="font-semibold text-lg">{title}</p>
+        <p class="mt-2">{description}</p>
+        <div class="mt-auto flex space-x-4 items-center">
+            <img class="w-10" src="/assets/github.svg" alt="Github Logo" />
+            <img class="w-10" src="/assets/lang-{lang}.svg" alt="{lang} Logo" />
+        </div>
+    </div>
+</a>
