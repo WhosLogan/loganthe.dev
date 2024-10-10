06b84d66758308b415b8ec8b066ce0211cf35b77
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-09 21:55:22 -0400

    Add basic social media link

---
+++ src/lib/SocialMediaLink.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let title;
+    export let link;
+    export let image;
+</script>
+
+<a href="{link}" target="_blank" rel="noopener noreferrer" class="hover:bg-neutral-700/70 w-full flex items-center space-x-3 px-3 py-2 rounded">
+    <img class="max-w-8" src={image} alt="{title} image" />
+    <p class="font-semibold">{title}</p>
+</a>
