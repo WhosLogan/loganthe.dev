21c82d8f0e458203d915bf9f8beaffe77a8eb086
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2024-10-09 22:22:39 -0400

    Finish contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import ContentBox from "$lib/ContentBox.svelte";
+</script>
+<div class="flex flex-col items-center mt-8">
+    <h1 class="mb-8 text-xl font-bold">Contact Me</h1>
+    <ContentBox title="Contact Info">
+        <p>You may contact me via the following methods</p>
+        <ul class="list-disc ml-4">
+            <li>&#x2709; <a href="mailto:logan@loganthe.dev">logan@loganthe.dev</a></li>
+            <li>&#x2709; <a href="mailto:logan@reallyprivate.email">logan@reallyprivate.email</a></li>
+        </ul>
+    </ContentBox>
+</div>
