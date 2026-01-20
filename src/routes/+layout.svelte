414e0585925a06e9d53e051aa72c34d970c7efa3
WhosLogan <128313631+WhosLogan@users.noreply.github.com>
2026-01-20 13:40:14 -0500

    Add main element

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 </svelte:head>
 
 <div class="min-h-screen flex flex-col">
-    <div class="mb-4">
+    <main class="mb-4">
         <div class="flex justify-center">
             <div class="flex flex-col md:flex-row md:space-x-40 mt-5 border-b w-3/4 md:w-1/2
         items-center md:justify-center pb-3 border-white/60">
@@ -23,7 +23,7 @@
             </div>
         </div>
         {@render children?.()}
-    </div>
+    </main>
     <footer class="mt-auto items-center justify-center flex h-10 w-full">
         <p class="text-center text-white/90">Created with &hearts; by Logan</p>
     </footer>
